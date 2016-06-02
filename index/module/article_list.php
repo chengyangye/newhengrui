da9aff74219e27f47aba6624026f16c792861d1b
yechengyang <yechengyang@yechengyangdeMac-mini.local>
2016-06-02 18:40:27 +0800

    文章属于子分类的时候显示文章分类

--- index/module/article_list.php
+++ index/module/article_list.php
@@ -16,13 +16,21 @@ function module_article_list()
 			$best_cat[$i]['channel'] = get_data('channel',$best_cat[$i]['cat_channel_id'],'cha_code');
 			$family = implode(',',get_cat_family('cat_art',$cat_id));
 			$obj = new article();
-			$obj->set_field('art_id,art_title,art_add_time');
+			$obj->set_field('art_id,art_title,art_add_time,art_cat_id');
 			$obj->set_where("art_cat_id in ($family)");
 			$obj->set_page_size($list_len ? $list_len : 5);
 			$list = $obj->get_list();
 			for($j = 0; $j < count($list); $j ++)
 			{
 				$list[$j]['short_title'] = cut_str($list[$j]['art_title'],22);
+				
+					$cat_parent_id = get_data('cat_art',$list[$j]['art_cat_id'],'cat_parent_id');
+					if($cat_parent_id!=0){
+						$list[$j]['cat_name'] = get_data('cat_art',$list[$j]['art_cat_id'],'cat_name').":";
+					}else{
+						$list[$j]['cat_name'] = "";
+					}
+					
 			}
 			$art_list[$cat_id] = $list;
 			unset($obj);
--- templates/default/index/module/article_list.php
+++ templates/default/index/module/article_list.php
@@ -5,10 +5,9 @@
 		{if $smarty.foreach.best_art_cat.index % 2 == 0}<div class="wrap mt-10"><div class="index_center ">{/if}
 		<div class="index_list_news l">
 			<h2 class="list_news_title">{$cat.cat_name} <span>  <a href="{url channel=$cat.channel cat=$cat.cat_id}">更多</a>  </span></h2>
-
 			<ul>
 				{foreach from=$art_list[$cat.cat_id] name=art_list item=item}
-				<li><a title="{$item.art_title}" target="_blank" href="{url channel=$cat.channel id=$item.art_id}">{$item.short_title}</a><span>{$item.art_add_time|date_format:"%Y-%m-%d"}</span></li>
+					<li><a title="{$item.art_title}" target="_blank" href="{url channel=$cat.channel id=$item.art_id}">{$item.cat_name}{$item.short_title}</a><span>{$item.art_add_time|date_format:"%Y-%m-%d"}</span></li>
 				{/foreach}
 			</ul>
 
